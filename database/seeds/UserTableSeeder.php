c108725897f5fad6d0110fcbeb12d94864a2cdc5
Unknown <arthur.delamare1@viacesi.fr>
2018-04-10 15:22:42 +0200

    ADD Seeders FOR Activities, Pictures, Users

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        for($i=0; $i<20; $i++){
+            $user = User::create([
+                'name' => $faker->firstName,
+                'email' => $faker->email,
+                'password' => $faker->password,
+                'surname' => $faker->lastName,
+                'rank' => $faker->numberBetween(0,2),
+            ]);
+
+        }
+    }
+}
