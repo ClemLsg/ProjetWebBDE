01bbbba803d18a9664a6ee1b09f31cd95dbbebf7
Unknown <arthur.delamare1@viacesi.fr>
2018-04-10 16:28:16 +0200

    ADD ProductTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PicturesTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(ActivitiesSeeder::class);
+        $this->call(ProductsTableSeeder::class);
     }
 }
