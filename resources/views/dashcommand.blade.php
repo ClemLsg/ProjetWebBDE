131715b64101c5a8a8cd7ddc9dc47cac105f7932
Gabgabga <gabriel.lesourd@viacesi.fr>
2018-04-12 16:46:53 +0200

    Modal du swagg

--- resources/views/dashcommand.blade.php
+++ resources/views/dashcommand.blade.php
@@ -44,7 +44,7 @@
                             <td>Bonet Ces'ESport</td>
                             <td>5</td>
                             <td>Paiement éspece en attente <i class="fa fa-money" aria-hidden="true"></i></td>
-                            <td><button type="button" class="btn btn-default" aria-label="Left Align">
+                            <td><button type="button" class="btn btn-default" aria-label="Left Align" data-toggle="modal" data-target="#change">
                                     <span class="fa fa-pencil-square-o fa-lg fa-2x" aria-hidden="true"></span>
                                 </button></td>
 
@@ -58,13 +58,56 @@
                             <td>Pull Ces'ESport</td>
                             <td>1</td>
                             <td>Paiement validé <i class="fa fa-paypal" aria-hidden="true"></i></td>
-                            <td><button type="button" class="btn btn-default" aria-label="Left Align">
-                                    <span class="fa fa-pencil-square-o fa-lg fa-2x" aria-hidden="true"></span>
+                            <td><button type="button" class="btn btn-default" aria-label="Left Align" data-toggle="modal" data-target="#change">
+                                    <span class="fa fa-pencil-square-o fa-lg fa-2x" aria-hidden="true">
+
+                                    </span>
                                 </button></td>
 
                         </tr>
                         </tbody>
                     </table>
+                </div>
+                <div class="modal fade" id="change" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Changer status</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                                    <label class="form-check-label" for="exampleRadios1">
+                                        Paiement validé
+                                        <i class="fa fa-paypal" aria-hidden="true"></i>
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                                    <label class="form-check-label" for="exampleRadios1">
+                                        Paiement en attente
+                                        <i class="fa fa-money" aria-hidden="true"></i>
+
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                                    <label class="form-check-label" for="exampleRadios1">
+                                        Terminé
+                                        <i class="fa fa-check" aria-hidden="true"></i>
+
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="card-footer small text-muted">Listes des commandes à jour sans délais</div>
             </div>
--- resources/views/manageproduct.blade.php
+++ resources/views/manageproduct.blade.php
@@ -36,7 +36,7 @@
                             <td>Vaseline Ces'ESport</td>
                             <td>465</td>
                             <td>20</td>
-                            <td><button type="button" class="btn btn-default" aria-label="Left Align">
+                            <td><button type="button" class="btn btn-default" aria-label="Left Align"  data-toggle="modal" data-target="#change">
                                     <span class="fa fa-pencil-square-o fa-lg fa-2x" aria-hidden="true"></span>
                                 </button></td>
 
@@ -46,8 +46,8 @@
                             <td>Pull Ces'ESport</td>
                             <td>154</td>
                             <td>20</td>
-                            <td><button type="button" class="btn btn-default" aria-label="Left Align">
-                                    <span class="fa fa-pencil-square-o fa-lg fa-2x" aria-hidden="true"></span>
+                            <td><button type="button" class="btn btn-default" aria-label="Left Align" data-toggle="modal" data-target="#change">
+                                    <span class="fa fa-pencil-square-o fa-lg fa-2x" aria-hidden="true" ></span>
                                 </button></td>
 
                         </tr>
@@ -56,28 +56,75 @@
                             <td>Pull Ces'ESport</td>
                             <td>465</td>
                             <td>20</td>
-                            <td><button type="button" class="btn btn-default" aria-label="Left Align">
+                            <td><button type="button" class="btn btn-default" aria-label="Left Align"  data-toggle="modal" data-target="#change">
                                     <span class="fa fa-pencil-square-o fa-lg fa-2x" aria-hidden="true"></span>
                                 </button></td>
 
                         </tr>
                         </tbody>
                     </table>
-                </div>
-                <div class="card-footer">
-                    <div class="container">
-                        <div class="row justify-content-md-center">
-                            <div class="col col-lg-2">
+                    <div class="card-footer">
+                        <div class="container">
+                            <div class="row justify-content-md-center">
+                                <div class="col col-lg-2">
 
-                            </div>
-                            <div class="col-md-auto">
-                                <button class="btn btn-lg btn-block btn-success text-uppercase">Ajouter un produit <span class="fa fa-plus" aria-hidden="true"></span></button>
-                            </div>
-                            <div class="col col-lg-2">
+                                </div>
+                                <div class="col-md-auto">
+                                    <button class="btn btn-lg btn-block btn-success text-uppercase">Ajouter un produit <span class="fa fa-plus" aria-hidden="true"></span></button>
+                                </div>
+                                <div class="col col-lg-2">
 
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div class="modal fade" id="change" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Changer status</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1"></span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="Stock" aria-label="Stock" aria-describedby="basic-addon1">
+                                    </div>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1"></span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="Ventes" aria-label="Ventes" aria-describedby="basic-addon1">
+                                    </div>
+                                </div>
+                                <div class="container">
+                                    <div class="row justify-content-md-center">
+                                        <div class="col col-lg-2">
+                                        </div>
+                                        <div class="col-md-auto">
+                                            <form class="upload">
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlFile1">Upload</label>
+                                                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="col col-lg-2">
+
+                                        </div>
+                                    </div>
+
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
